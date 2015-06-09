191c820bd15b8556a85e799792f6d0506d127391
pitrackster <pitrackster@gmail.com>
2015-06-09 15:09:24 +0200

    changed upload file dir

--- Manager/MediaManager.php
+++ Manager/MediaManager.php
@@ -4,7 +4,6 @@ namespace Innova\MediaResourceBundle\Manager;
 
 use Doctrine\ORM\EntityManager;
 use Innova\MediaResourceBundle\Entity\MediaResource;
-use Innova\MediaResourceBundle\Entity\Media;
 
 /**
  * Media Manager
@@ -12,13 +11,9 @@ use Innova\MediaResourceBundle\Entity\Media;
 class MediaManager {
 
     protected $em;
-    protected $readFileDir;
-    protected $uploadFileDir;
 
-    public function __construct(EntityManager $em, $readFileDir, $uploadfileDir) {
+    public function __construct(EntityManager $em) {
         $this->em = $em;
-        $this->readFileDir = $readFileDir;
-        $this->uploadFileDir = $uploadfileDir;
     }
 
     public function getRepository() {
@@ -28,7 +23,7 @@ class MediaManager {
     public function getAudioMediaUrl(MediaResource $mr) {
         $audio = $this->getRepository()->findOneBy(array('mediaResource' => $mr, 'type' => 'audio'));
         if ($audio) {
-            return $this->getReadFileDirectory() . '/' . $audio->getUrl();
+            return $audio->getUrl();
         }
         return null;
     }
@@ -36,18 +31,44 @@ class MediaManager {
     public function getVideoMedia(MediaResource $mr) {
         $video = $this->getRepository()->findOneBy(array('mediaResource' => $mr, 'type' => 'video'));
         if ($video) {
-            return $this->getReadFileDirectory() . '/' . $video->getUrl();
+            return $video->getUrl();
         }
         return null;
     }
-
-
-    protected function getReadFileDirectory() {
-        return $this->readFileDir;
+/*
+    public function upload(UploadedFile $file, $url) {
+        if (null === $file) {
+            return;
+        }
+        $uploaded = $file->move($this->getUploadRootDir(), $url);
+        unset($file);
+        return $uploaded;
     }
 
-    protected function getUploadDirectory() {
-        return $this->uploadFileDir;
+    public function removeUpload($filename) {
+        $url = $this->getUploadRootDir() . '/' . $filename;
+        if (file_exists($url)) {
+            unlink($url);
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    public function getAbsolutePath() {
+        return null === $this->getUrl() ? null : $this->getUploadRootDir() . '/' . $this->url;
+    }
+
+    public function getWebPath() {
+        return null === $this->getUrl() ? null : $this->getUploadDir() . '/' . $this->url;
+    }
+
+    protected function getUploadRootDir() {
+        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
+    }
+
+    protected function getUploadDir() {
+        return 'media/uploads';
+    }
+*/
 }
